5767731b83d79e095141908c8fec824f4c681003
Akron254 <aaronrono42@gmail.com>
2021-01-19 21:41:20 +0300

    Reusable header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context, {bool isAppTitle, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'ChatPen' : titleText,
+      style: isAppTitle
+          ? TextStyle(
+              color: Colors.white,
+              fontFamily: 'Signatra',
+              fontSize: 50.0,
+            )
+          : TextStyle(
+              color: Colors.white,
+            ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
